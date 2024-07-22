5512c542054dd0facf0935e4003e91d5ecbddcb4
Gayathiri-Jeyananthan <gayathirijeyananthan@gmail.com>
2024-07-22 13:01:09 +0530

    First update 22nd July

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+import Signup from '../src/Components/Signup.jsx'
+import Login from '../src/Components/Login.jsx'
+
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/register' element={<Signup/>}/>
+        <Route path='/login' element={<Login/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
